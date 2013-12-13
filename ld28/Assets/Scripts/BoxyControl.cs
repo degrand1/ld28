1b826446644288fef419e10549e889855c306539
Nathan DeGrand <degrand1@illinois.edu>
2013-12-12 22:57:14 -0800

    Added scrolling, jumping, and sweet back flips off the sides of boxes
    Turned the edge collider into a box collider. Added a ground check to
    tell when the player could jump.

--- ld28/Assets/Scripts/BoxyControl.cs
+++ ld28/Assets/Scripts/BoxyControl.cs
@@ -6,13 +6,29 @@ public class BoxyControl : MonoBehaviour
     public float antiMoveForce = 200f;
     public float maxSpeed = 5f;
 
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+	private bool jump = false;
+	public bool grounded = false;
+
     void Awake()
     {
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
     }
 
     void Update()
     {
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+		
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
     }
+	
+	// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+	//if(h * rigidbody2D.velocity.x < maxSpeed)
 
     void FixedUpdate()
     {
@@ -23,11 +39,16 @@ public class BoxyControl : MonoBehaviour
         if ( Mathf.Abs( rigidbody2D.velocity.x ) > maxSpeed ) {
             rigidbody2D.velocity = new Vector2( Mathf.Sign( rigidbody2D.velocity.x ) * maxSpeed, rigidbody2D.velocity.y );
         }
-
+		
         // do not let player slide. Apply resistance.
         if ( Mathf.Abs( h ) < Mathf.Epsilon && Mathf.Abs( rigidbody2D.velocity.x ) > maxSpeed / 3 ) {
             rigidbody2D.AddForce( Vector2.right * -Mathf.Sign( rigidbody2D.velocity.x ) * antiMoveForce );
         }
 
+		if( jump ){
+			// Add a vertical force to the player.
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
     }
 }
--- ld28/Assets/Scripts/Reference/CameraFollow.cs
+++ ld28/Assets/Scripts/Reference/CameraFollow.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour 
 {
-	public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
-	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
-	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
-	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
-	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
-	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
+	public float xMargin = 2f;		// Distance in the x axis the player can move before the camera follows.
+	public float yMargin = 2f;		// Distance in the y axis the player can move before the camera follows.
+	public float xSmooth = 2f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	public float ySmooth = 2f;		// How smoothly the camera catches up with it's target movement in the y axis.
+	public Vector2 maxXAndY;  		// The maximum x and y coordinates the camera can have.
+	public Vector2 minXAndY;  		// The minimum x and y coordinates the camera can have.
 
 
-	private Transform player;		// Reference to the player's transform.
+	public Transform player;		// Reference to the player's transform.
 
 
 	void Awake ()
