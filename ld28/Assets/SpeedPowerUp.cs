284e602f5e2f3f4494796b360f64e0f5d0355696
Nathan DeGrand <degrand1@illinois.edu>
2013-12-14 20:52:05 -0800

    Added an animation controller for the power up.

---
+++ ld28/Assets/SpeedPowerUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPowerUp : MonoBehaviour {
+
+	public TriggerPowerUpAnimation animationController;
+
+	// Use this for initialization
+	void Start () {
+		// Setting up the reference.
+		animationController = GameObject.FindGameObjectWithTag("PowerUpSprite").GetComponent<TriggerPowerUpAnimation>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		// If the player enters the trigger zone...
+		if(other.tag == "Player")
+		{
+			animationController.StartSpeedAnimation();
+			// Destroy me
+			Destroy( transform.root.gameObject );
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
